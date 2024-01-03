cde538f994b2668ab41d52a5c75ba5c296788393
Omer Taitelman <omer.taitelman@post.runi.ac.il>
2024-01-03 21:02:10 +0200

    changed prompt without example

--- pyGenerator.py
+++ pyGenerator.py
@@ -30,28 +30,14 @@ PROGRAMS_LIST=[
         {"name": "kth smallest element in BST", "prompt": "A program that finds the kth smallest element in a given binary search tree."}
 ]
 
-temperature=0.6
+temperature=0.7
 
 def get_user_input():
     user_input = input("Tell me, which program would you like me to code for you?"\
               +" If you don't have an idea,just press enter and I will choose a random program to code\n")
     if not user_input:
         # chooses random program to generate from PROGRAMS_LIST
-        user_input = {"name": "interleavings of two strings", "prompt": '''Given two strings str1 and str2, prints all interleavings of the given
-        two strings. You may assume that all characters in both strings are
-        different.Input: str1 = "AB", str2 = "CD"
-        Output:
-        ABCD
-        ACBD
-        ACDB
-        CABD
-        CADB
-        CDAB
-        Input: str1 = "AB", str2 = "C"
-        Output:
-        ABC
-        ACB
-        CAB "'''}##random.choice(PROGRAMS_LIST)
+        user_input = random.choice(PROGRAMS_LIST)
         print(color.Fore.YELLOW + "generating: ", user_input["name"])
         user_input = user_input["prompt"]
     else:
@@ -73,18 +59,56 @@ def get_initial_messages(user_input):
     #     "content": "You are an expert python developer that cannot speak at all, you provide just python code"
       
     # }
+    example = '''an example of generating such code for the prompt: "make a calculator with the operation +":
+import sys
+def program(operation, a, b):
+    try:
+        if operation == '+':
+            answer = a + b
+            return answer
+        else:
+            return "Invalid operation"
+    except TypeError:
+        return "Invalid input. Please enter a number."
+
+def unit_tests():
+    # Test for addition
+    assert program('+', 3, 4) == 7.0
+    assert program('+', 100000000, 100000000) == 200000000.0
+    assert program('+', 0, 0) == 0.0
+    # Test for invalid operation
+    assert program('-', 3, 4) == "Invalid operation", "Invalid operation test failed"
+
+    # Test for invalid input
+    assert program('+', 'three', 4) == "Invalid input. Please enter a number.", "Invalid input test failed"
+    return True
+                                   
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == "test":
+        try:
+            unit_tests()
+            print("all tests passed")
+        except Exception as e:
+            raise e
+    else:
+        a = float(input("Enter the first number: "))
+        b = float(input("Enter the second number: "))
+        print(str(program("+", a, b)))
+
+if __name__ == "__main__":
+    main()'''
     initial_user_message = {
                             "role": "user",
                             "content": f'''You are an expert python developer that cannot speak at all, you provide just python code
-                                      Create for me a python program. 
-                                        {user_input} 
-                                      refer to this code as the 'program'. 
-                                      Also please include unit tests (refer to it as the 'unit_tests') that check the logic of the program using 5 different inputs and expected outputs. 
-                                      The 'unit_tests' should call the 'program' code in order to test properly."
-                                      the code should contain two functions: the 'program' and the 'unit_tests' and also main function that check program's Command Line Argument (sys.argv).  
-                                      if program's Command Line Argument is 'test' then run the unit tests. the program should return True if they all passed, and raise an exception if one of the test failes.
-                                      if the program's Command Line Argument (sys.argv) is empty, run the 'program' code.   
-                                      also keep in mind that the 'unit_tests' should use the 'program' code when testing.''',
+Create python program for: 
+  {user_input}.
+refer to this code as the 'program'. 
+include unit tests (refer to it as the 'unit_tests') that check the logic of the program using 5 different inputs and expected outputs. 
+The 'unit_tests' should call the 'program' code in order to test properly."
+the code should contain three functions: the 'program', 'unit_tests' and also main function that check program's Command Line Argument (sys.argv).  
+if program's Command Line Argument is 'test' then run the unit tests. the program should return True if they all passed, and raise an exception if one of the test failes.
+if the program's Command Line Argument (sys.argv) is empty, run the 'program' code.   
+''',
                         }
 
     messages = [initial_user_message]
@@ -113,7 +137,7 @@ def generate_code( messages, client ):
     with open(file_name) as f:
         code_generated = f.read()
     code_generated_message = {"role": "assistant",
-                        "content": code_generated}
+                                "content": code_generated}
     print(f"---------------- Testing {file_name} ----------------")
     test_result, response = run_and_test_code(file_name=file_name)
     return test_result, response, code_generated_message
@@ -127,14 +151,15 @@ def create_file(file_name, code_generated):
 def run_and_test_code(file_name):
     try: 
         test_result = subprocess.run(f"python {file_name} test", capture_output=True, text=True)
-        print(test_result)
         if(test_result.returncode == 1):
-            print(color.Fore.RED + f"Error running generated code! Error:{test_result.stderr}")
+            full_traceback = test_result.stderr
+            short_traceback = "line " + full_traceback.split("File")[-1].split("line")[-1]
+            print(color.Fore.RED + f"Error running generated code! Error:{short_traceback}")
             return False, {"role": "user",
                             "content": "Error running unit_tests." \
-                                        +  "please fix it and provide again the full code" + test_result.stderr}
+                                        +  "please fix it and provide again the full code" + short_traceback}
         elif("fail" in test_result.stdout.lower()):
-            print(test_result.stdout)
+            print(color.Fore.RED + f"Error running generated code! Error:{test_result.stdout}")
             return False, {"role": "user",
                             "content": "unit_tests doesnt raise excaption when one of the tests failed." \
                                         + "please fix it and provide again the full code"}
@@ -142,10 +167,10 @@ def run_and_test_code(file_name):
             print(test_result.stdout)
             return True,"Code creation completed successfully!"
         
-    except Exception as e :
+    except:
         full_traceback = traceback.format_exc()
         short_traceback = "line " + full_traceback.split("File")[-1].split("line")[-1]
-        print(color.Fore.RED + f"Error running generated code! Error:{short_traceback}")
+        print(color.Fore.RED + f"excaption Error running generated code! Error:{short_traceback}")
        
         return False, {"role": "user",
                             "content": "Error running unit_tests!." \
