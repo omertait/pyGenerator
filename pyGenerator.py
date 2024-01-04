8782a9dcd7677b1b881d5f647af3f8af893408eb
Omer Taitelman <omer.taitelman@post.runi.ac.il>
2024-01-04 14:18:50 +0200

    loading with threads

--- pyGenerator.py
+++ pyGenerator.py
@@ -5,12 +5,15 @@ import traceback
 from dotenv import load_dotenv
 from openai import OpenAI
 import colorama as color
-import tqdm as tqdm  # without line it's working
+import time
+from threading import Thread
 
 load_dotenv()
-file_name="generatedcode.py"
-PROGRAMS_LIST=[
-        {"name": "interleavings of two strings", "prompt": '''Given two strings str1 and str2, prints all interleavings of the given
+file_name = "generatedcode.py"
+PROGRAMS_LIST = [
+    {
+        "name": "interleavings of two strings",
+        "prompt": '''Given two strings str1 and str2, prints all interleavings of the given
         two strings. You may assume that all characters in both strings are
         different.Input: str1 = "AB", str2 = "CD"
         Output:
@@ -24,17 +27,28 @@ PROGRAMS_LIST=[
         Output:
         ABC
         ACB
-        CAB "'''},
-        {"name": "check number is palindrom", "prompt" : "a program that checks if a number is a palindrome"}
-        ,
-        {"name": "kth smallest element in BST", "prompt": "A program that finds the kth smallest element in a given binary search tree."}
+        CAB "''',
+    },
+    {
+        "name": "check number is palindrom",
+        "prompt": "a program that checks if a number is a palindrome",
+    },
+    {
+        "name": "kth smallest element in BST",
+        "prompt": "A program that finds the kth smallest element in a given binary search tree.",
+    },
 ]
 
-temperature=0.7
+temperature = 0.7
+
+def parse_tracback(full_traceback):
+    return "line " + full_traceback.split("File")[-1].split("line")[-1]
 
 def get_user_input():
-    user_input = input("Tell me, which program would you like me to code for you?"\
-              +" If you don't have an idea,just press enter and I will choose a random program to code\n")
+    user_input = input(color.Fore.WHITE +
+        "Tell me, which program would you like me to code for you?"
+        + " If you don't have an idea,just press enter and I will choose a random program to code\n"
+    )
     if not user_input:
         # chooses random program to generate from PROGRAMS_LIST
         user_input = random.choice(PROGRAMS_LIST)
@@ -44,22 +58,28 @@ def get_user_input():
         print(color.Fore.YELLOW + "generating your code ")
     return user_input
 
+
 def init_OpenAi_client():
     client = OpenAI(
         api_key=os.environ.get("OPENAI_API_KEY"),
     )
     return client
 
-
+def get_GPT_Response(client, messages, temperature, chat_completion):
+    chat_completion[0] = client.chat.completions.create(
+        messages=messages,
+        model="gpt-3.5-turbo",
+        temperature=temperature,
+    )
 
 def get_initial_messages(user_input):
     # initial_system_role = {
-        
+
     #     "role": "system",
     #     "content": "You are an expert python developer that cannot speak at all, you provide just python code"
-      
+
     # }
-    example = '''an example of generating such code for the prompt: "make a calculator with the operation +":
+    example = """an example of generating such code for the prompt: "make a calculator with the operation +":
 import sys
 def program(operation, a, b):
     try:
@@ -96,10 +116,10 @@ def main():
         print(str(program("+", a, b)))
 
 if __name__ == "__main__":
-    main()'''
+    main()"""
     initial_user_message = {
-                            "role": "user",
-                            "content": f'''You are an expert python developer that cannot speak at all, you provide just python code
+        "role": "user",
+        "content": f"""You are an expert python developer that cannot speak at all, you provide just python code
 Create python program for: 
   {user_input}.
 refer to this code as the 'program'. 
@@ -108,75 +128,94 @@ The 'unit_tests' should call the 'program' code in order to test properly."
 the code should contain three functions: the 'program', 'unit_tests' and also main function that check program's Command Line Argument (sys.argv).  
 if program's Command Line Argument is 'test' then run the unit tests. the program should return True if they all passed, and raise an exception if one of the test failes.
 if the program's Command Line Argument (sys.argv) is empty, run the 'program' code.   
-''',
-                        }
+""",
+    }
 
     messages = [initial_user_message]
     return messages
 
-def generate_code( messages, client ):
+
+def generate_code(messages, client):
     print(f"---------------- Generating Code ----------------")
-    chat_completion = client.chat.completions.create(
-        messages=messages,
-        model="gpt-3.5-turbo",
-        temperature=temperature,
-    )
+    chat_completion = [None]
+    call_API_thread = Thread(target=get_GPT_Response, args=(client,messages, temperature, chat_completion))
+    call_API_thread.start()
+
+
+    while Thread.is_alive(call_API_thread):
+        time.sleep(1)
+        print(". ", end=" ", flush=True)
+
+    call_API_thread.join()
+    print(" ")
+    
     try:
+        chat_completion = chat_completion[0]
         code_generated = chat_completion.choices[0].message.content
         if "```python" in code_generated:
             code_generated = code_generated.split("```")[1].split("python")[1]
     except Exception as e:
         print(color.Fore.RED + "error with response from model")
         print(color.Fore.RED + e)
-      
-    
+
     print(f"---------------- Creating File {file_name} ----------------")
     create_file(code_generated=code_generated, file_name=file_name)
+
     print(f"---------------- auto-format {file_name} using black ----------------")
     subprocess.run(f"black {file_name}")
     with open(file_name) as f:
         code_generated = f.read()
-    code_generated_message = {"role": "assistant",
-                                "content": code_generated}
+    # set code_generated_message as the code after black auto format
+    code_generated_message = {"role": "assistant", "content": code_generated}
+
     print(f"---------------- Testing {file_name} ----------------")
-    test_result, response = run_and_test_code(file_name=file_name)
-    return test_result, response, code_generated_message
-   
+    test_result, response, traceback = run_and_test_code(file_name=file_name)
+    return test_result, response, code_generated_message, traceback
+
 
 def create_file(file_name, code_generated):
     # Writing the code to the file
     with open(file_name, "w") as file:
         file.write(code_generated)
 
+
 def run_and_test_code(file_name):
-    try: 
-        test_result = subprocess.run(f"python {file_name} test", capture_output=True, text=True)
-        if(test_result.returncode == 1):
+    try:
+        test_result = subprocess.run(
+            f"python {file_name} test", capture_output=True, text=True
+        )
+        # tests failed
+        if test_result.returncode == 1:
             full_traceback = test_result.stderr
-            short_traceback = "line " + full_traceback.split("File")[-1].split("line")[-1]
-            print(color.Fore.RED + f"Error running generated code! Error:{short_traceback}")
-            return False, {"role": "user",
-                            "content": "Error running unit_tests." \
-                                        +  "please fix it and provide again the full code" + short_traceback}
-        elif("fail" in test_result.stdout.lower()):
-            print(color.Fore.RED + f"Error running generated code! Error:{test_result.stdout}")
-            return False, {"role": "user",
-                            "content": "unit_tests doesnt raise excaption when one of the tests failed." \
-                                        + "please fix it and provide again the full code"}
-        else:
-            print(test_result.stdout)
-            return True,"Code creation completed successfully!"
+            short_traceback = parse_tracback(full_traceback)
+            return False, {
+                "role": "user",
+                "content": "Error running unit_tests."
+                + "please fix it and provide again the full code"
+                + short_traceback,
+            }, short_traceback
         
+        # tests failed but it was printed into stdout
+        elif "fail" in test_result.stdout.lower():
+            return False, {
+                "role": "user",
+                "content": f"unit_tests doesnt raise excaption when one of the tests failed. it prints to stdout instead : {test_result.stdout}"
+                + "please fix it and provide again the full code",
+            }, test_result.stdout
+        
+        else:
+            # All test Passed
+            return True, "Code creation completed successfully!", None
+
     except:
         full_traceback = traceback.format_exc()
-        short_traceback = "line " + full_traceback.split("File")[-1].split("line")[-1]
-        print(color.Fore.RED + f"excaption Error running generated code! Error:{short_traceback}")
-       
-        return False, {"role": "user",
-                            "content": "Error running unit_tests!." \
-                                        +  "please fix it and provide again the full code" + short_traceback}
-
-
+        short_traceback = parse_tracback(full_traceback)
+        return False, {
+            "role": "user",
+            "content": "Error running unit_tests!."
+            + "please fix it and provide again the full code"
+            + short_traceback,
+        }, short_traceback
 
 
 def main():
@@ -184,27 +223,27 @@ def main():
     client = init_OpenAi_client()
     messages = get_initial_messages(user_input)
     for i in range(5):
-        if i > 0: ## not the first iteration
-            print(color.Fore.YELLOW +  "---------------- fixing errors ----------------")
+        if i > 0:  ## not the first iteration
+            print(color.Fore.YELLOW + "---------------- fixing errors ----------------")
 
-        result = generate_code(messages, client)
-        
-        test_result, response, code_generated_message = result
-        
+        test_result, response, code_generated_message, traceback = generate_code(messages, client)
+
         if test_result:
             print(color.Fore.GREEN + response)
+            print(color.Fore.YELLOW + "---------------- Running Generated Code ----------------")
+            print(color.Fore.WHITE)
             subprocess.run(f"python {file_name}")
             break
         else:
+            print(
+                color.Fore.RED
+                + f"excaption Error running generated code! Error:{traceback}"
+            )
             messages.extend([code_generated_message, response])
-   
+
     if not test_result:
         print(color.Fore.RED + "Code generation FAILED")
-        
-    
 
 
-
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
